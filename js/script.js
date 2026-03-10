19bbf687c48c596c92b84e76bcf9a374c9dbc845
MykolaBrunarskyi <mykola.brunarskyi.pp.2023@lpnu.ua>
2026-03-10 17:55:59 +0200

    feat: implement peek method for stack

--- js/script.js
+++ js/script.js
@@ -7,6 +7,11 @@ class Stack {
         this.items.push(element);
     }
 
+    peek() {
+        if (this.items.length === 0) return "Стек порожній";
+        return this.items[this.items.length - 1];
+    }
+    
     pop() {
         if (this.items.length === 0) return "Стек порожній";
         return this.items.pop();
